d71e95619509f442e8860a460b3653007561a7bc
Chelsie Conrad <chelsiechey@gmail.com>
2020-06-11 00:03:58 -0600

    auth0 added

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -10,7 +10,8 @@
           Make sure you check out our upcoming events below
         </h2>
         <div class="button-block">
-          <button class="button is-xl is-dark">Sign Up to Browse Events</button>
+          <button v-if="!$auth.isAuthenticated" @click="login" class="button is-xl is-dark">Sign Up to Browse Events</button>
+          <h3 v-if="$auth.isAuthenticated" class="is-size-3 has-background-dark welcome">Welcome, {{ $auth.user.name }}!</h3>
         </div>
       </div>
     </div>
@@ -21,11 +22,17 @@
 <script>
   import EventsList from '../components/EventsList';
   export default {
-    name: 'home',
-    components: {
-      EventsList
+  name: 'home',
+  components: {
+    EventsList
+  },
+  methods: {
+    // Log the user in
+    login() {
+      this.$auth.loginWithRedirect();
     }
   }
+}
 </script>
 <style lang="scss" scoped>
   .hero {  
